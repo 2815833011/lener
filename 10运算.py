dc00660121d326f22119fc053acd772b674f02ef
tangying <2815833011@qq.com>
2025-02-06 20:18:49 +0800

    10运算符

--- 10运算.py
+++ 10运算.py
@@ -1,2 +1,38 @@
-#sdhfiehsidf 
-#@三角地飞机上
+#算数运算
+a=10
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+#赋值运算 变量=值
+a =100
+c =a-b
+print(c)
+
+#比较运算
+a=100
+b=200
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+#逻辑运算
+#and or
+print(True and False,"and")
+print(True or False,"or")
+
+#身份运算 is  is not
+a="hahaha"
+b="hahaha"
+print(a is b,"is")
+print(a is not b,"is not")
+
+print(id(a),id(b))
